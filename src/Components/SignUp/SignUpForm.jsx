03ba545309d1b00c9eb396a18c17e79b357e2d85
ifty03 <ashikulislamb84@gmail.com>
2025-02-12 11:48:36 +0600

    create signup page

--- src/Components/SignUp/SignUpForm.jsx
+++ src/Components/SignUp/SignUpForm.jsx
@@ -5,12 +5,14 @@ import { BsPerson } from "react-icons/bs";
 import Typography from "../Typography";
 import { CiKeyboard, CiLock } from "react-icons/ci";
 import { IoEyeOffOutline, IoEyeOutline } from "react-icons/io5";
-import { Checkbox } from "@/components/ui/checkbox";
+import { IoMdPhonePortrait } from "react-icons/io";
+import { MdOutlineAlternateEmail } from "react-icons/md";
 
-const SignInForm = () => {
+const SignUpForm = () => {
   const [showPassword, setShowPassword] = useState(false);
-  const [number, setNumber] = useState("");
+  const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
   const [otp, setOtp] = useState("");
   const [showResendOtp, setShowResendOtp] = useState(false);
   const [otpMessage, setOtpMessage] = useState("");
@@ -30,7 +32,7 @@ const SignInForm = () => {
 
   return (
     <form
-      className="  rounded-3xl px-8 pt-8 pb-5 secondary-border p-10 bg-white max-w-xl mx-auto border"
+      className="  rounded-3xl px-8 pt-8 pb-5 gradient-border p-10 bg-white max-w-xl mx-auto border"
       onSubmit={handleSubmit}
     >
       <div className="w-fit mx-auto pb-8 font-font-pop">
@@ -38,28 +40,56 @@ const SignInForm = () => {
           className="text-black text-center"
           variant="semibold"
         >
-          Login to your Account
+          Create an account
         </Typography.Heading3>
       </div>
 
       {!otpSent ? (
         <div className="space-y-4 mb-6 ">
           <div>
-            <label className="text-black mb-2 block text-lg">
-              Enter your mobile number
-            </label>
+            <label className="text-black mb-2 block text-lg">Full name</label>
             <div className="relative">
               <input
                 className="w-full bg-primary/5 outline-none border border-gray-300 px-5 py-3  sm:text-[16px] rounded-full pl-10 text-black"
-                id="number"
-                type="number"
-                placeholder="Enter Your mobile number"
-                value={number}
-                onChange={(e) => setNumber(e.target.value)}
+                id="fullName"
+                name="fullName"
+                type="text"
+                placeholder="Enter your full name"
               />
               <BsPerson className="pointer-events-none absolute inset-y-0 start-1 top-3.5 grid w-10 place-content-center text-2xl text-gray-400" />
             </div>
           </div>
+          <div>
+            <label className="text-black mb-2 block text-lg">
+              Email address
+            </label>
+            <div className="relative">
+              <input
+                className="w-full bg-primary/5 outline-none border border-gray-300 px-5 py-3  sm:text-[16px] rounded-full pl-10 text-black"
+                id="email"
+                name="email"
+                type="email"
+                placeholder="Enter your email address"
+              />
+              <MdOutlineAlternateEmail className="pointer-events-none absolute inset-y-0 start-1 top-3.5 grid w-10 place-content-center text-2xl text-gray-400" />
+            </div>
+          </div>
+          <div>
+            <label className="text-black mb-2 block text-lg">
+              Enter mobile number
+            </label>
+            <div className="relative">
+              <input
+                className="w-full bg-primary/5 outline-none border border-gray-300 px-5 py-3  sm:text-[16px] rounded-full pl-10 text-black"
+                id="phone"
+                name="phone"
+                type="number"
+                placeholder="Enter your mobile number"
+              />
+              <IoMdPhonePortrait className="pointer-events-none absolute inset-y-0 start-1 top-3.5 grid w-10 place-content-center text-2xl text-gray-400" />
+            </div>
+          </div>
+
           <div>
             <label className="text-black mb-2 block text-lg"> Password </label>
             <div className="relative">
@@ -86,16 +116,34 @@ const SignInForm = () => {
               </div>
             </div>
           </div>
-
-          {/* Remember Me & Forgot Password */}
-          <div className="flex justify-between items-center mb-6  ">
-            <div className="flex items-center gap-2">
-              <Checkbox className=""></Checkbox>
-              <span className=""> Remember me</span>
+          <div>
+            <label className="text-black mb-2 block text-lg">
+              {" "}
+              Confirm password{" "}
+            </label>
+            <div className="relative">
+              <input
+                className="w-full  bg-primary/5 outline-none border border-gray-300 px-5 py-3  sm:text-[16px] rounded-full pl-10 text-black"
+                id="password"
+                type={showPassword ? "text" : "password"}
+                placeholder="Re-enter your password"
+                value={confirmPassword}
+                onChange={(e) => setConfirmPassword(e.target.value)}
+              />
+              <div className="absolute left-3 top-1/2 -translate-y-1/2">
+                <CiLock className="text-secondaryText/60 text-2xl" />
+              </div>
+              <div
+                className="absolute right-3 top-1/2 -translate-y-1/2 cursor-pointer"
+                onClick={handleTogglePassword}
+              >
+                {showPassword ? (
+                  <IoEyeOffOutline className="text-secondaryText/60 text-2xl" />
+                ) : (
+                  <IoEyeOutline className="text-secondaryText/60 text-2xl" />
+                )}
+              </div>
             </div>
-            <Link to="/forgotPassword" className="hover:underline">
-              Forgot Password?
-            </Link>
           </div>
         </div>
       ) : (
@@ -126,19 +174,19 @@ const SignInForm = () => {
           >
             {isLoading && <div>Loading........</div>}
             {!isLoading && otpSent && "Verify OTP"}
-            {!isLoading && !otpSent && "Sign In"}
+            {!isLoading && !otpSent && "Sign Up"}
           </span>
           {/* group-hover:top-0 group-hover:-right-9 */}
         </button>
 
         <Typography.Body variant="normal" className="text-black/70 text-center">
-          You don’t have any account yet?{" "}
+          Already have an account?{" "}
         </Typography.Body>
         <Link
-          to="/sign-up"
+          to="/login"
           className="text-secondary text-center block font-semibold"
         >
-          Create an account
+          Login here
         </Link>
       </div>
       {otpSent && (
@@ -158,4 +206,4 @@ const SignInForm = () => {
   );
 };
 
-export default SignInForm;
+export default SignUpForm;
---
+++ src/Pages/SignUpPage.jsx
@@ -0,0 +1,13 @@
+import SignUp from "@/Components/SignUp/SignUp";
+import Navbar from "@/Shared/Navbar/Navbar";
+
+const SignUpPage = () => {
+  return (
+    <div>
+      <Navbar />
+      <SignUp />
+    </div>
+  );
+};
+
+export default SignUpPage;
