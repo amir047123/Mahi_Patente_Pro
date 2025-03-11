76272b34b41bf44ab37906b4ed2ea49160d1dbe9
Shimul Hossain <techdevshimul@gmail.com>
2025-03-11 21:46:42 +0600

    Fixed Loading Issue Of Quizzes

--- src/Pages/UserDashboard/UserDasboardChooseSignal.jsx
+++ src/Pages/UserDashboard/UserDasboardChooseSignal.jsx
@@ -248,184 +248,177 @@ const UserDasboardChooseSignal = () => {
           { name: "Choose 4 to 1 Signal", path: "quiz/choose-4-to-1-signal" },
         ]}
       />
-      {isLoading ? (
-        <div className="py-40 flex items-center justify-center">
-          <Spinner size={60} />
-        </div>
-      ) : (
-        <>
-          <div className={`${isSummary ? "hidden" : ""}`}>
-            <div className="col-span-1 md:col-span-3 grid grid-cols-5 md:grid-cols-10 gap-2 bg-[#E5E7EB] p-3 rounded-lg my-5">
-              {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((num) => (
-                <button
-                  onClick={() => setCurrentPosition(num)}
-                  key={num}
-                  className={`rounded-lg bg-white py-1.5 font-semibold text-2xl shadow-sm text-center 
+
+      <div className={`${isSummary ? "hidden" : ""}`}>
+        <div className="col-span-1 md:col-span-3 grid grid-cols-5 md:grid-cols-10 gap-2 bg-[#E5E7EB] p-3 rounded-lg my-5">
+          {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((num) => (
+            <button
+              onClick={() => setCurrentPosition(num)}
+              key={num}
+              className={`rounded-lg bg-white py-1.5 font-semibold text-2xl shadow-sm text-center 
                 ${
                   num === currentPosition
                     ? " text-[#CB2A8A]"
                     : " text-[#9CA3AF]"
                 }`}
+            >
+              {num}
+            </button>
+          ))}
+        </div>
+        {quizSession?.quizzes && quizSession?.quizzes?.length > 0 && (
+          <div className="bg-white rounded-2xl px-5 pb-10 col-span-2">
+            <div className="flex items-center justify-between md:p-4 p-2 border-b">
+              <div className="flex items-center md:gap-5 gap-2">
+                <Typography.Body
+                  variant="medium"
+                  className="text-secondaryText whitespace-nowrap"
                 >
-                  {num}
+                  Question No:
+                </Typography.Body>
+                <button className="px-3 py-2 font-semibold rounded-md border text-secondary">
+                  {currentPosition}
+                </button>
+              </div>
+
+              {hasTimer && (
+                <div className="flex items-center md:gap-5 gap-2">
+                  <Typography.Body
+                    variant="medium"
+                    className="text-secondaryText whitespace-nowrap"
+                  >
+                    <span className="hidden lg:inline-block">
+                      Remaining Time:
+                    </span>
+                  </Typography.Body>
+                  <span className="px-3 font-semibold whitespace-nowrap md:text-xl text-base py-1.5 rounded-sm bg-[#FEF3C7] flex items-center gap-2">
+                    {/* <Clock size={20} /> <QuizTimer /> */}
+                    <ClockAlert size={20} /> {formatTime()}
+                  </span>
+                </div>
+              )}
+            </div>
+
+            <Typography.Body
+              variant="semibold"
+              className="text-secondaryText my-4"
+            >
+              Select the right signal
+            </Typography.Body>
+            <Typography.Body variant="nomal" className="text-primaryText my-4">
+              {currentQuiz?.question}
+            </Typography.Body>
+
+            <div className="grid md:grid-cols-4 grid-cols-2 md:gap-5 gap-3 mt-8">
+              {currentQuiz?.options?.map((option, i) => (
+                <button
+                  key={i}
+                  onClick={() =>
+                    currentQuiz?.answer === undefined &&
+                    handleAnswer(i, currentQuiz?._id)
+                  }
+                  className={`py-3 px-7 rounded-lg w-full ${
+                    currentQuiz?.answer === i
+                      ? "text-[#22AD5C] bg-[#EAFEEA] border border-[#22AD5C]"
+                      : "text-primaryText bg-white border"
+                  }`}
+                >
+                  <img src={option} alt="" />
                 </button>
               ))}
             </div>
-            {quizSession?.quizzes && quizSession?.quizzes?.length > 0 && (
-              <div className="bg-white rounded-2xl px-5 pb-10 col-span-2">
-                <div className="flex items-center justify-between md:p-4 p-2 border-b">
-                  <div className="flex items-center md:gap-5 gap-2">
-                    <Typography.Body
-                      variant="medium"
-                      className="text-secondaryText whitespace-nowrap"
-                    >
-                      Question No:
-                    </Typography.Body>
-                    <button className="px-3 py-2 font-semibold rounded-md border text-secondary">
-                      {currentPosition}
-                    </button>
-                  </div>
+          </div>
+        )}
+        <div className="col-span-1 md:col-span-3 flex justify-between mt-5">
+          <div className="flex space-x-2">
+            <button className="bg-white rounded-lg px-4 py-2 font-medium shadow-sm flex items-center text-red-600 gap-2">
+              <IoIosCloseCircleOutline className="text-xl" />
 
-                  {hasTimer && (
-                    <div className="flex items-center md:gap-5 gap-2">
-                      <Typography.Body
-                        variant="medium"
-                        className="text-secondaryText whitespace-nowrap"
-                      >
-                        <span className="hidden lg:inline-block">
-                          Remaining Time:
-                        </span>
-                      </Typography.Body>
-                      <span className="px-3 font-semibold whitespace-nowrap md:text-xl text-base py-1.5 rounded-sm bg-[#FEF3C7] flex items-center gap-2">
-                        {/* <Clock size={20} /> <QuizTimer /> */}
-                        <ClockAlert size={20} /> {formatTime()}
-                      </span>
-                    </div>
-                  )}
-                </div>
+              <span className="hidden lg:inline-block">Close</span>
+            </button>
+            <button
+              onClick={() => setIsSummary(true)}
+              className="bg-white rounded-lg px-4 py-2 shadow-sm flex items-center text-gray-600 gap-2 font-medium"
+            >
+              <AlignJustify className="w-4 h-4 mr-1" />
 
-                <Typography.Body
-                  variant="semibold"
-                  className="text-secondaryText my-4"
-                >
-                  Select the right signal
-                </Typography.Body>
-                <Typography.Body
-                  variant="nomal"
-                  className="text-primaryText my-4"
-                >
-                  {currentQuiz?.question}
-                </Typography.Body>
-
-                <div className="grid md:grid-cols-4 grid-cols-2 md:gap-5 gap-3 mt-8">
-                  {currentQuiz?.options?.map((option, i) => (
-                    <button
-                      key={i}
-                      onClick={() => handleAnswer(i, currentQuiz?._id)}
-                      className={`py-3 px-7 rounded-lg w-full ${
-                        currentQuiz?.answer === i
-                          ? "text-[#22AD5C] bg-[#EAFEEA] border border-[#22AD5C]"
-                          : "text-primaryText bg-white border"
-                      }`}
-                    >
-                      <img src={option} alt="" />
-                    </button>
-                  ))}
-                </div>
-              </div>
-            )}
-            <div className="col-span-1 md:col-span-3 flex justify-between mt-5">
-              <div className="flex space-x-2">
-                <button className="bg-white rounded-lg px-4 py-2 font-medium shadow-sm flex items-center text-red-600 gap-2">
-                  <IoIosCloseCircleOutline className="text-xl" />
-
-                  <span className="hidden lg:inline-block">Close</span>
-                </button>
-                <button
-                  onClick={() => setIsSummary(true)}
-                  className="bg-white rounded-lg px-4 py-2 shadow-sm flex items-center text-gray-600 gap-2 font-medium"
-                >
-                  <AlignJustify className="w-4 h-4 mr-1" />
-
-                  <span className="hidden lg:inline-block">Summary</span>
-                </button>
-              </div>
-
-              <div className="flex space-x-2">
-                <button
-                  onClick={handlePrevious}
-                  className="bg-white rounded-lg px-4 py-2 shadow-sm flex items-center text-gray-600 font-medium"
-                >
-                  <ChevronLeft className="w-4 h-4 mr-1" />
-
-                  <span className="hidden lg:inline-block">Prev</span>
-                </button>
-                <button
-                  onClick={handleNext}
-                  className="bg-white rounded-lg px-4 py-2 shadow-sm flex items-center text-gray-600 font-medium"
-                >
-                  <span className="hidden lg:inline-block">Next</span>
-                  <ChevronRight className="w-4 h-4 ml-1" />
-                </button>
-
-                <button
-                  onClick={handleSubmit}
-                  className="bg-green-500 text-white rounded-lg px-4 py-2 shadow-sm flex items-center font-medium justify-center disabled:bg-green-500/70 disabled:cursor-not-allowed"
-                  disabled={isSubmitting}
-                >
-                  {isSubmitting ? (
-                    <Spinner size={20} className="text-white" />
-                  ) : (
-                    <>
-                      <span className="hidden lg:inline-block">Submit</span>
-                      <MdDone className="w-4 h-4 ml-1" />
-                    </>
-                  )}
-                </button>
-              </div>
-            </div>
+              <span className="hidden lg:inline-block">Summary</span>
+            </button>
           </div>
 
-          <UserDashboardChooseSignalSummary
-            handleSubmit={handleSubmit}
-            isSubmitting={isSubmitting}
-            showAnswer={showAnswer}
-            hasTimer={hasTimer}
-            time={time}
-            isSummary={isSummary}
-            setIsSummary={setIsSummary}
-            quizzes={quizSession?.quizzes}
-          />
-          <TimeLeftModal
-            setIsSummary={setIsSummary}
-            submitAnswers={submitAnswers}
-            isSubmitting={isSubmitting}
-            time={time}
-            isOpen={isTimeLeftModalOpen}
-            setIsOpen={setIsTimeLeftModalOpen}
-          />
-          <QuestionLeftModal
-            setIsSummary={setIsSummary}
-            submitAnswers={submitAnswers}
-            isSubmitting={isSubmitting}
-            skippedAnswer={skippedAnswer}
-            isOpen={isQuestionLeftModalOpen}
-            setIsOpen={setIsQuestionLeftModalOpen}
-          />
-          <QuickSettingsModal
-            showAnswer={showAnswer}
-            setShowAnswer={setShowAnswer}
-            hasTimer={hasTimer}
-            setHasTimer={setHasTimer}
-            isLoading={isLoading}
-            getQuizzes={getQuizzes}
-            isOpen={isQuickSettingsModalOpen}
-            setIsOpen={setIsQuickSettingsModalOpen}
-            difficulty={difficulty}
-            setDifficulty={setDifficulty}
-          />
-        </>
-      )}
+          <div className="flex space-x-2">
+            <button
+              onClick={handlePrevious}
+              className="bg-white rounded-lg px-4 py-2 shadow-sm flex items-center text-gray-600 font-medium"
+            >
+              <ChevronLeft className="w-4 h-4 mr-1" />
+
+              <span className="hidden lg:inline-block">Prev</span>
+            </button>
+            <button
+              onClick={handleNext}
+              className="bg-white rounded-lg px-4 py-2 shadow-sm flex items-center text-gray-600 font-medium"
+            >
+              <span className="hidden lg:inline-block">Next</span>
+              <ChevronRight className="w-4 h-4 ml-1" />
+            </button>
+
+            <button
+              onClick={handleSubmit}
+              className="bg-green-500 text-white rounded-lg px-4 py-2 shadow-sm flex items-center font-medium justify-center disabled:bg-green-500/70 disabled:cursor-not-allowed"
+              disabled={isSubmitting}
+            >
+              {isSubmitting ? (
+                <Spinner size={20} className="text-white" />
+              ) : (
+                <>
+                  <span className="hidden lg:inline-block">Submit</span>
+                  <MdDone className="w-4 h-4 ml-1" />
+                </>
+              )}
+            </button>
+          </div>
+        </div>
+      </div>
+
+      <UserDashboardChooseSignalSummary
+        handleSubmit={handleSubmit}
+        isSubmitting={isSubmitting}
+        showAnswer={showAnswer}
+        hasTimer={hasTimer}
+        time={time}
+        isSummary={isSummary}
+        setIsSummary={setIsSummary}
+        quizzes={quizSession?.quizzes}
+      />
+      <TimeLeftModal
+        setIsSummary={setIsSummary}
+        submitAnswers={submitAnswers}
+        isSubmitting={isSubmitting}
+        time={time}
+        isOpen={isTimeLeftModalOpen}
+        setIsOpen={setIsTimeLeftModalOpen}
+      />
+      <QuestionLeftModal
+        setIsSummary={setIsSummary}
+        submitAnswers={submitAnswers}
+        isSubmitting={isSubmitting}
+        skippedAnswer={skippedAnswer}
+        isOpen={isQuestionLeftModalOpen}
+        setIsOpen={setIsQuestionLeftModalOpen}
+      />
+      <QuickSettingsModal
+        showAnswer={showAnswer}
+        setShowAnswer={setShowAnswer}
+        hasTimer={hasTimer}
+        setHasTimer={setHasTimer}
+        isLoading={isLoading}
+        getQuizzes={getQuizzes}
+        isOpen={isQuickSettingsModalOpen}
+        setIsOpen={setIsQuickSettingsModalOpen}
+        difficulty={difficulty}
+        setDifficulty={setDifficulty}
+      />
     </div>
   );
 };
