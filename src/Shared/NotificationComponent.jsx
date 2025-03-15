4afb4420493949cdb8d99a1ed75ede123cd089ea
Shimul Hossain <techdevshimul@gmail.com>
2025-03-15 15:50:25 +0600

    Updated Notification Design

--- src/Shared/NotificationComponent.jsx
+++ src/Shared/NotificationComponent.jsx
@@ -17,13 +17,13 @@ const NotificationComponent = () => {
   return (
     <div className="min-w-80 max-w-[395px] py-2 bg-[#ECF2F8] border-2 rounded-lg shadow-xl absolute top-8 -right-20 lg:-right-32 z-[9999] mx-auto">
       <div className="">
-        <div className="flex justify-between items-center p-4 border-b">
+        <div className="flex justify-between items-center px-4 py-2 border-b">
           <h2 className="text-xl font-semibold text-secondary">
             Notifications
           </h2>
           <button
             type="button"
-            className="flex gap-2 text-primary hover:text-primary/80 text-xs"
+            className="flex gap-2 text-primary hover:text-primary/80 text-xs py-2"
             onClick={markAllAsRead}
           >
             <CheckCheck size={16} />
@@ -63,8 +63,8 @@ const NotificationComponent = () => {
                     className="mb-2"
                   >
                     <Link
-                      className={`flex items-center gap-3 p-2 mx-4 my-2 rounded-xl bg-white hover:bg-gray-50 transition-colors ${
-                        !notification.read ? "bg-red-50" : ""
+                      className={`flex items-center gap-3 p-2 mx-4 my-2 rounded-xl  hover:bg-gray-50 transition-colors ${
+                        !notification.read ? "bg-red-50" : "bg-white"
                       }`}
                       to={handleUrl()}
                     >
@@ -79,17 +79,23 @@ const NotificationComponent = () => {
                           alt="leaderboard notification"
                         />
                       ) : (
-                        <FaBell className="p-2 rounded-full size-12 bg-gray-100 text-gray-600" />
+                        <FaBell className="p-2 rounded-full size-10 bg-gray-100 text-gray-600" />
                       )}
 
                       <div className="flex-1 min-w-0">
-                        <p className="text-sm font-medium text-gray-900">
+                        <p
+                          className={`text-sm ${
+                            !notification.read
+                              ? "font-semibold text-primaryText"
+                              : "font-medium text-secondaryText"
+                          }`}
+                        >
                           {notification?.title}
                         </p>
-                        <p className="text-sm text-gray-500">
+                        <p className="text-xs text-gray-500">
                           {notification?.message}
                         </p>
-                        <p className="text-xs text-gray-400 mt-1">
+                        <p className="text-xs text-secondary mt-1">
                           <time>
                             {new Date(notification?.createdAt).toLocaleString(
                               "en-GB",
@@ -116,6 +122,12 @@ const NotificationComponent = () => {
                           </time>
                         </p>
                       </div>
+
+                      {!notification.read && (
+                        <div className="flex items-center">
+                          <div className="w-2 h-2 bg-secondary rounded-full" />
+                        </div>
+                      )}
                     </Link>
                   </div>
                 );
