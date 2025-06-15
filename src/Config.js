b75d0b59006b9906e37107cd39853cabd0cbcc07
Shimul Hossain <techdevshimul@gmail.com>
2025-06-15 20:15:58 +0600

    chore: config update for localhost

--- src/Config.js
+++ src/Config.js
@@ -1,4 +1,4 @@
-// export const mainURL = "http://localhost:3003";
-export const mainURL = "https://server-mahipatentepro.com";
+export const mainURL = "http://localhost:3003";
+// export const mainURL = "https://server-mahipatentepro.com";
 
 export const baseURL = `${mainURL}/api/v1`;
